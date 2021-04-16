55282e605dafeea26b2827e33e0c283d5fcb73eb
Hydra455 <77670576+Hydra455@users.noreply.github.com>
2021-04-16 17:21:54 +0300

    Update c_possaver.lua

--- c_possaver.lua
+++ c_possaver.lua
@@ -2,34 +2,35 @@ local path = nil
 local recording = false
 
 function a()
-if recording == false then
-recording = true
-outputChatBox("true")
-if fileExists("save.txt") then
-path = fileOpen("save.txt")
-outputChatBox("event started")
-addEventHandler("onClientRender", getRootElement(), record)
-else
-path = fileCreate("save.txt")
-removeEventHandler("onClientRender", getRootElement(), record)
-outputChatBox("event removed")
-end
-else
-fileClose(path)
-outputChatBox("false")
-removeEventHandler("onClientRender", getRootElement(), record)
-recording = false
-end
-end
-bindKey("F5", "down", a)
+    if recording == false then
+        recording = true
+        outputChatBox("Starting...")
+        if fileExists("save.txt") then
+            path = fileOpen("save.txt")
+            outputChatBox("The script began to write the coordinates")
+            outputChatBox("Press 'F5' again if you want to stop")
+            addEventHandler("onClientRender", getRootElement(), record)
+        else
+            path = fileCreate("save.txt")
+            removeEventHandler("onClientRender", getRootElement(), record)
+            outputChatBox("The script no longer writes the coordinates")
+        end
+    else
+        fileClose(path)
+        outputChatBox("Stopped")
+        removeEventHandler("onClientRender", getRootElement(), record)
+        recording = false
+        end
+    end
+    bindKey("F5", "down", a)
 
-function record()
-local x, y, z = getElementPosition(localPlayer)
-if recording then
-fileWrite(path, ""..x..", "..y..", "..z.."")
-else
-end
-end
+    function record()
+        local x, y, z = getElementPosition(localPlayer)
+        if recording then
+            fileWrite(path, "\n"..x..","..y..","..z.."")
+        else
+        end
+        end
 
 
 
@@ -39,3 +40,4 @@ end
 
 
 --// The .txt file will save in the client mods folder
+--// By Hydra (Last edited 16/04/2021)
